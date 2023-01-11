4fdaa93340df1c96c1542a182fa3792cbd372744
Marco D'Addona <marco.daddona@t-online.de>
2023-01-11 17:07:56 +0100

    V.1

---
+++ Biomarkt-Software/LoadingScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biomarkt_Software
+{
+    public partial class LoadingScreen : Form
+    {
+
+        private int loadingBarValue = 0;
+
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingbarTimer.Start();
+        }
+
+        private void loadingbarTimer_Tick(object sender, EventArgs e)
+        {
+
+            loadingBarValue += 1;
+            labelLoadingProgress.Text = loadingBarValue.ToString() + " %";
+            loadingProgressbar.Value = loadingBarValue;
+        
+            if(loadingBarValue >= loadingProgressbar.Maximum)
+            {
+                loadingbarTimer.Stop();
+
+                //Finished Loading
+            }
+        }
+
+    }
+}
